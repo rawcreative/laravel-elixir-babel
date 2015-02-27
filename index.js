0e6d71fec937a3170467325d3a36c718c8c9e593
T. Debo <tim@rawcreativestudios.com>
2015-02-27 09:52:25 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps'),
+    gulpIf = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    _ = require('underscore'),
+    elixir = require('laravel-elixir'),
+    utilities = require('laravel-elixir/ingredients/commands/Utilities'),
+    notification = require('laravel-elixir/ingredients/commands/Notification');
+
+elixir.extend('babel', function (src, options) {
+
+    var config = this,
+        defaultOptions = {
+            debug:         ! config.production,
+            srcDir:        config.assetsDir + 'js',
+            output:        config.jsOutput,
+            sourceMaps: false
+        };
+
+    options = _.extend(defaultOptions, options);
+    src = "./" + utilities.buildGulpSrc(src, options.srcDir);
+
+    gulp.task('babel', function () {
+
+        var onError = function(e) {
+            new notification().error(e, 'Babel Compilation Failed!');
+            this.emit('end');
+        };
+
+        return gulp.src(src)
+            .pipe(gulpIf(options.sourceMaps, sourcemaps.init()))
+            .pipe(babel(options)).on('error', onError)
+            .pipe(gulpIf(! options.debug, uglify()))
+            .pipe(gulpIf(options.sourceMaps, sourcemaps.write('.')))
+            .pipe(gulp.dest(options.output))
+            .pipe(new notification().message('Babel Compilation Finished!'));
+    });
+
+    this.registerWatcher('babel', options.srcDir + '/**/*.js');
+
+    return this.queueTask('babel');
+
+});
